24a7df8f78689b118a8eaae634c1044646b709e8
Matija Kočevar <matija.kocev@gmail.com>
2022-08-02 00:35:32 +0200

    adds dropdown for inputs

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,58 +1,13 @@
-import { useState } from 'react'
-import  SelectMIDIDevice  from './components/SelectMIDIDevice';
-import './App.css'
+import { useState } from "react";
+import SelectMIDIDevice from "./components/SelectMIDIDevice";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
-  if (navigator.requestMIDIAccess) {
-    console.log('This browser supports WebMIDI!');
-} else {
-    console.log('WebMIDI is not supported in this browser.');
-}
-
-
-
-navigator.requestMIDIAccess()
-    .then(onMIDISuccess, onMIDIFailure);
-
-function onMIDISuccess(midiAccess) {
-    console.log(midiAccess);
-
-    midiAccess.onstatechange = (e) => {
-
-      console.log('Some deivce connected!: ', e);
-    }
-
-    var inputs = midiAccess.inputs;
-    var outputs = midiAccess.outputs;
-
-
-    inputs = [ ...inputs.values() ]
-
-    console.log(inputs[1])
-
- //   inputs[1].onmidimessage = (m) => console.log('Recevied midi message: ', m.data)
-
-
-  
-    console.log("inputs: ", inputs);
-    console.log("outputs: ", outputs);
-
-
-}
-
-function onMIDIFailure() {
-    console.log('Could not access your MIDI devices.');
-}
-
   return (
     <div className="App">
       <SelectMIDIDevice></SelectMIDIDevice>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- ClientApp/src/components/SelectMIDIDevice.tsx
+++ ClientApp/src/components/SelectMIDIDevice.tsx
@@ -1,21 +1,59 @@
+import { BaseSyntheticEvent, useEffect, useState } from "react";
+
 const SelectMIDIDevice = () => {
+  const [inputs, setInputs] = useState<WebMidi.MIDIInput[]>();
+  const [selectedInput, setSelectedInput] = useState<string>();
+
+  useEffect(() => {
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+    function onMIDISuccess(midiAccess: WebMidi.MIDIAccess) {
+      midiAccess.onstatechange = (e: any) => {
+        console.log("Some device connected!: ", e);
+      };
+
+      setInputs(Array.from(midiAccess.inputs.values()));
+    }
+
+    function onMIDIFailure() {
+      console.log("Could not access your MIDI devices.");
+    }
+  }, []);
+
+  useEffect(() => {
+    // if (inputs) console.log(inputs);
+    if (selectedInput) console.log(selectedInput);
+  }, [inputs, selectedInput]);
+
   return (
     <>
       <label
-        htmlFor="countries"
+        htmlFor="inputs"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
       >
         Select an option
       </label>
       <select
-        id="countries"
+        id="inputs"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        defaultValue={"default"}
+        onChange={(e: BaseSyntheticEvent) => {
+          setSelectedInput(e.currentTarget.value);
+        }}
       >
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
+        <>
+          <option disabled hidden value={"default"}>
+            Select an input
+          </option>
+          {inputs?.length &&
+            inputs.map((input) => {
+              return (
+                <option key={input.id} value={input.id}>
+                  {input.name}
+                </option>
+              );
+            })}
+        </>
       </select>
     </>
   );
